1adfb855a70cd3dd3f9ac093d064997cecc6c9bf
Scott Simpson <sasimpson@gmail.com>
2025-08-15 11:10:39 -0500

    update main to have a graceful shutdown

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"tokenize/api"
 	"tokenize/persistence/dynamodb"
 )
 
-func main() {
+func buildServer() *http.Server {
 	db := dynamodb.CreateLocalClient()
 
 	dynamodb.SetupDynamoTable(context.Background(), db)
@@ -19,15 +25,39 @@ func main() {
 		},
 	}
 	routes := api.Routes(handlers)
-	server := &http.Server{
+	return &http.Server{
 		Addr:    ":8080",
 		Handler: routes,
 	}
 
+}
+
+func main() {
+	server := buildServer()
+
+	shutdownChan := make(chan bool, 1)
+
 	fmt.Println("starting service on", server.Addr)
-	err := server.ListenAndServe()
-	if err != nil {
-		panic(err)
+	go func() {
+		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("http server error: %v\n", err)
+		}
+		time.Sleep(1 * time.Millisecond)
+		log.Println("stopped serving new connections")
+		shutdownChan <- true
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 10+time.Second)
+	defer shutdownRelease()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("http shutdown error: %v\n", err)
 	}
 
+	<-shutdownChan
+	log.Println("graceful shutdown complete")
 }
